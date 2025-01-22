af4758a230efae519586414f6721a200d61d9090
Shubham Gupta <sgupta@docquity.com>
2025-01-23 00:10:26 +0530

    Listing screen data updated.

--- feature/composite/src/main/java/com/sgupta/composite/listing/NewsListItem.kt
+++ feature/composite/src/main/java/com/sgupta/composite/listing/NewsListItem.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,8 +13,6 @@ import androidx.navigation.NavHostController
 import com.sgupta.composite.listing.components.NewsListItem
 import com.sgupta.composite.listing.states.NewsListViewState
 import com.sgupta.core.components.LoadingIndicator
-import com.sgupta.core.components.SearchBar
-import com.sgupta.core.components.SectionHeadline
 import com.sgupta.core.components.ToolbarComposable
 
 @Composable
@@ -37,14 +34,7 @@ fun NewsList(
         when {
             state.newsUiModel?.isNotEmpty() == true -> {
                 LazyColumn(modifier = Modifier.fillMaxSize()) {
-                    item {
-                        SearchBar(Modifier.padding(horizontal = 16.dp))
-                    }
-                    item {
-                        SectionHeadline(Modifier, "Top Headlines")
-                    }
                     items(state.newsUiModel.size) { index ->
-//                        ArticleListItem(state.newsUiModel[index])
                         NewsListItem(state.newsUiModel[index])
                     }
                 }
